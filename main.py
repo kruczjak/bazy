09d48d1d4afbb4ed654b2b86326b1aee3581743d
kruczjak <kruczjak@gmail.com>
2015-01-16 22:03:29 +0100

    Add people and confreservation

--- main.py
+++ main.py
@@ -60,14 +60,49 @@ class Workshops:
     def list(self, confdaylist):
         list=[]
         f = open("data/workshop.csv")
-        dict = {}
-        for content in f.readlines():
-            dict = {}
-            splitted = content.split('|')
-            dict['name'] = splitted[0]
-            dict['start_time'] = splitted[1]
 
-            list.append(dict)
+        for row in confdaylist:
+            for i in range(0, 4):
+                dict = {}
+                splitted = f.readline().strip().split('|')
+                dict['name'] = splitted[0]
+                dict['start_date'] = row[2]
+                dict['start_time'] = splitted[1] + ":" + splitted[2]
+                dict['end_time'] = "3 hours"
+                dict['seats'] = splitted[3]
+                dict['price'] = splitted[4]
+                dict['id_ConfDay'] = row[0]
+                list.append(dict)
+        return list
+
+class ConfReservations:
+    def list(self, confdaylist):
+        list=[]
+        for row in confdaylist:
+            for i in range(0, 2):
+                dict = {}
+                dict['id_User'] = random.randint(1, 200)
+                dict['id_ConfDay'] = row[0]
+                dict['reserved_seats'] = int(row[1] / 2)
+                list.append(dict)
+        return list
+
+class People:
+    def list(self, cdr):
+        list=[]
+        f = open("data/people.csv")
+        for row in cdr:
+            for i in range(0, row[3]):
+                dict = {}
+                splitted = f.readline().strip().split('|')
+                dict['first_name'] = splitted[0]
+                dict['last_name'] = splitted[1]
+                dict['id_cdr'] = row[0]
+                if random.randint(0, 10)==0:
+                    dict['student_card']=123123
+                else:
+                    dict['student_card']=None
+                list.append(dict)
         return list
 
 # MAIN PROGRAM
@@ -111,14 +146,37 @@ conn.commit()
 
 cur.execute("""SELECT * FROM \"ConfDay\"""")
 list = cur.fetchall()
-print(list)
 try:
     cur.executemany(
-        """SELECT add_workshop(%(id_ConfDay)s, %(name)s, %(start_time)s, %(end_time)s, %(seats)s, %(price)s)""",
+        """SELECT add_workshop(%(id_ConfDay)s, %(name)s, date %(start_date)s + time %(start_time)s, date %(start_date)s + time %(start_time)s + interval %(end_time)s, %(seats)s, %(price)s)""",
         Workshops().list(list))
+    conn.commit()
 except:
     print("Pomijam Workshop")
     print(traceback.print_exc())
+
+try:
+    cur.executemany(
+        """SELECT create_confreservation(%(id_User)s, %(id_ConfDay)s, %(reserved_seats)s)""",
+        ConfReservations().list(list)
+    )
+    conn.commit()
+except:
+    print("Pomijam Confreservation")
+    print(traceback.print_exc())
+
+cur.execute("""SELECT * FROM \"ConfDayReservation\"""")
+cdr = cur.fetchall()
+try:
+    cur.executemany(
+        """SELECT connect_person_to_conference(%(first_name)s, %(last_name)s, %(id_cdr)s, %(student_card)s)""",
+        People().list(cdr)
+    )
+    conn.commit()
+except:
+    print("Pomijam People")
+    print(traceback.print_exc())
+
 
 conn.commit()
 cur.close()
